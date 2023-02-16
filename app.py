4a5bca479de52738d0a59055533668f51a0d0c8e
Shreyas Marda <shreyasmtex@gmail.com>
2023-02-16 15:00:42 +0530

    This web app is ready

--- app.py
+++ app.py
@@ -23,7 +23,17 @@ def predict_api():
     output = regmodel.predict(new_data)
     # print(output[0])
     return jsonify(output[0])
-    # return list([0])
+    
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(numpy.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text = "The house price prediction is {}".format(output))
+    # return [1]
+
+
 
 if __name__== "__main__":
     app.run(debug = True)
